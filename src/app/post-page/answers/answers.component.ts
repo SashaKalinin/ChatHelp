b070875cfb881005572ef60c21790fa9f0b4af08
SashaKalinin <sashakalininweb@gmail.com>
2020-09-12 21:17:35 +0300

    Сверстал блок коментариев

--- src/app/post-page/answers/answers.component.ts
+++ src/app/post-page/answers/answers.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from '../../shared/services/auth.service';
+import {Post} from '../../../environments/interface';
+import {Subscription} from 'rxjs';
+import {PostService} from '../../shared/services/post.service';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -7,15 +11,32 @@ import {AuthService} from '../../shared/services/auth.service';
   templateUrl: './answers.component.html',
   styleUrls: ['./answers.component.less']
 })
-export class AnswersComponent implements OnInit {
+export class AnswersComponent implements OnInit, OnDestroy {
   checked = false;
+  author: string;
+  card: Post;
+  postSub: Subscription;
+  isLoaded = false;
   constructor(
     private authService: AuthService,
+    private postService: PostService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.author = this.authService.email;
+    this.postSub = this.postService.getQuestCard(this.router.url.slice(1)).subscribe(post => {
+      this.card = post;
+      this.isLoaded = true;
+    });
   }
 
   submit(): void {
+  }
+
+  ngOnDestroy(): void {
+    if (this.postSub) {
+      this.postSub.unsubscribe();
+    }
   }
 }
