eed3854e0683120c826c692ed8474709315dd511
SashaKalinin <sashakalininweb@gmail.com>
2020-09-27 21:48:45 +0300

    отрефакторил замечания Бориса по пайпу фильтр

--- src/app/shared/Pipes/filter-pipe.pipe.ts
+++ src/app/shared/Pipes/filter-pipe.pipe.ts
@@ -2,13 +2,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 import {Post} from '../../../environments/interface';
 
 @Pipe({
-  name: 'filterPipe'
+  name: 'filterPipe',
+  pure: false
 })
 export class FilterPipePipe implements PipeTransform {
 
   transform(posts: Post[], commentSelect: string[], direSelect?: string[], timeSelect?: string ): Post[] {
     let resArr: Post[] = posts;
     const filterTrigger: string[] = [];
+    const day = 86400000;
+    const week = 604800000;
+    const msInMonth = (33 - new Date(new Date().getFullYear(), new Date().getMonth(), 33).getDate()) * day;
+
     pushTrigger(commentSelect, filterTrigger, direSelect, timeSelect);
     if (posts.length === 0 || filterTrigger.length === 0) {
       return posts;
@@ -21,23 +26,17 @@ export class FilterPipePipe implements PipeTransform {
         if (trigger === 'Answered') {
           resArr = resArr.filter( post => post.complete);
         }
-        if (trigger === 'Frontend') {
-          resArr = filterDir(resArr, trigger);
-        }
-        if (trigger === '.NET') {
-          resArr = filterDir(resArr, trigger);
-        }
-        if (trigger === 'Salesforce') {
-          resArr = filterDir(resArr, trigger);
+        if (direSelect) {
+          direSelect.forEach(dir => resArr = filterDir(resArr, dir));
         }
         if (trigger === 'Per day') {
-          resArr = perDay(resArr);
+          resArr = timeFilter(resArr, day);
         }
         if (trigger === 'Per week') {
-          resArr = perWeek(resArr);
+          resArr = timeFilter(resArr, week);
         }
         if (trigger === 'Per month') {
-          resArr = perMonth(resArr);
+          resArr = timeFilter(resArr, msInMonth);
         }
       });
     }
@@ -45,42 +44,20 @@ export class FilterPipePipe implements PipeTransform {
   }
 }
 
-function perDay(arr: Post[]): Post[] {
+function timeFilter(arr: Post[], time: number): Post[] {
       arr = arr.filter(post => {
-        const day = 86400000;
         const dateNow = new Date().getTime();
-        if ((dateNow - post.date) < day) {
+        if ((dateNow - post.date) < time) {
           return post;
         }
       });
       return arr;
 }
-function perWeek(arr: Post[]): Post[] {
-    arr = arr.filter(post => {
-      const week = 604800000;
-      const dateNow = new Date().getTime();
-      if ((dateNow - post.date) < week) {
-        return post;
-      }
-    });
-    return arr;
-}
-function perMonth(arr: Post[]): Post[] {
-    arr = arr.filter(post => {
-      const msInMonth = (33 - new Date(new Date().getFullYear(), new Date().getMonth(), 33).getDate()) * 86400000;
-      const dateNow = new Date().getTime();
-      if ((dateNow - post.date) < msInMonth) {
-        return post;
-      }
-    });
-    return arr;
-}
 function filterDir(arr: Post[], trigger: string): Post[] {
     arr = arr.filter(post => {
-      let flag = false;
-      post.direct.forEach(dir => dir === trigger ? flag = true : flag);
+      const flag = post.direct.includes(trigger);
       if (flag) {
-        return post;
+        return true;
       }
     });
     return arr;
